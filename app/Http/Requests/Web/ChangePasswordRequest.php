05889c8ab7cf3b2cf25bdea115b41d14399063e3
Tomáš Dvořák <tomas.dvorak@geekcoders.cz>
2024-10-10 18:55:54 +0200

    TASK-01 - Non responsive user section FE

---
+++ app/Http/Requests/Web/ChangePasswordRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Web;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangePasswordRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'password' => ['required', 'confirmed'],
+            'password_confirmation' => ['required_with:password'],
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function messages(): array
+    {
+        return [
+            'password.confirmed' => 'Hesla se neshodují',
+            'password.required' => 'Heslo je povinné',
+            'password_confirmation.required_with' => 'Pro změnu hesla je nutno zadat heslo znovu'
+        ];
+    }
+}
---
+++ resources/views/livewire/web/simple-calendar.blade.php
@@ -0,0 +1,35 @@
+<div class="space-y-2 text-xl">
+    <div class="flex flex-row justify-between font-bold w-full mx-auto">
+        <svg wire:click="decreaseMonth" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        </svg>
+
+        <p>{{ $this->label() }}</p>
+
+        <svg wire:click="addMonth" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        </svg>
+    </div>
+
+    <table class="w-full text-center">
+        <tr>
+            <th class="px-1">PO</th>
+            <th class="px-1">ÚT</th>
+            <th class="px-1">ST</th>
+            <th class="px-1">ČT</th>
+            <th class="px-1">PÁ</th>
+            <th class="px-1">SO</th>
+            <th class="px-1">NE</th>
+        </tr>
+
+        @for($j = 0; $j < 6; $j++)
+           <tr>
+               @for($i = 0; $i < 7; $i++)
+                    <td>{{ $this->printDay()  }}</td>
+               @endfor
+           </tr>
+        @endfor
+
+    </table>
+
+</div>
