f7bb9213343ffade5517377d2cec25ab661bc274
Tomáš Dvořák <tomas.dvorak@geekcoders.cz>
2025-01-26 16:30:42 +0100

    TASK06-Reservations improvements, toast messages

--- routes/console.php
+++ routes/console.php
@@ -12,5 +12,4 @@ Schedule::command('model:prune', [
     '--model' => [ReservationTemp::class],
 ])->everyFifteenMinutes();
 
-Schedule::command('php artisan reservations:delete-expired')->everyFifteenMinutes();
 Schedule::command('php artisan reservations:send-upcoming-notification')->dailyAt(0);
