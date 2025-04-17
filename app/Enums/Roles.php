4794bcab93882f1def598fc1b30c133763059884
Tomáš Dvořák <tomas.dvorak@geekcoders.cz>
2025-04-17 14:48:58 +0200

    TASK09  - fixed flowbite and styling

--- app/Enums/Roles.php
+++ app/Enums/Roles.php
@@ -6,8 +6,6 @@ enum Roles: string
 {
     case ADMINISTRATOR = 'administrator';
     case USER = 'user';
-    //case SERVICES_ADMINISTRATOR = 'services_administrator';
-    //case LEAGUE_ADMINISTRATOR = 'league_administrator';
 
     public static function adminGroup(): array
     {
@@ -19,8 +17,6 @@ enum Roles: string
         return [
             'unselected' => 'Vybrat roli',
             self::ADMINISTRATOR->value => 'Administrátor',
-            //self::SERVICES_ADMINISTRATOR->value => 'Administrátor služeb',
-            //self::LEAGUE_ADMINISTRATOR->value => 'Administrátor ligy',
             self::USER->value => 'Uživatel',
         ];
     }
@@ -29,8 +25,6 @@ enum Roles: string
     {
         return match ($this) {
             self::ADMINISTRATOR => 'Administrátor',
-            // self::SERVICES_ADMINISTRATOR => 'Administrátor služeb',
-            // self::LEAGUE_ADMINISTRATOR => 'Administrátor ligy',
             self::USER => 'Uživatel',
         };
     }
